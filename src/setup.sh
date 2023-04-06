a0862c925c7c970a06582ec22fc76be79f67f655
Kattson Bastos <kattsonbastos@gmail.com>
2023-04-06 13:07:49 +0000

    build: config gitSync

--- src/setup.sh
+++ src/setup.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
+install() {
+    # kubectl
+    echo "## Installing kubectl.."
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-# kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    #heml
+    echo "## Installing helm.."
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
 
-# heml
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    echo "## Installing gcloud.."
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
-# gcloud
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    sudo apt update && sudo apt install google-cloud-cli
 
-sudo apt update && sudo apt install google-cloud-cli
+    sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+    #export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+}
 
-gcloud init
-
+case $1 in
+  install)
+    install
+    ;;
+  *)
+    echo "Usage: $0 {install}"
+    ;;
+esac
---
+++ src/setup_cluster.sh
@@ -0,0 +1,69 @@
+
+setup_cluster(){
+  # set project
+
+  echo "### Creating Cluster.."
+  gcloud config set project k-practices
+
+  # gcloud services list --available | grep container
+  gcloud services enable container.googleapis.com
+
+  gcloud container clusters create airflow-cluster \
+  --machine-type n1-standard-4 \
+  --num-nodes 1 \
+  --region "us-east1"
+
+  # authenticate kubectl
+  echo "### Getting creds kb"
+  gcloud container clusters get-credentials airflow-cluster --region "us-east1"
+
+  # create a namespace
+  kubectl create namespace airflow
+
+  # deploy airfflow
+  helm repo add apache-airflow https://airflow.apache.org
+
+  # list repo
+  helm repo list
+
+  # forward port
+  # kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow
+
+  echo "### Adding SSH"
+
+  mkdir ~/.ssh
+
+  #mv ./id_rsa.pub ~/.ssh
+
+  kubectl create secret generic airflow-gke-git-secret --from-file=gitSshKey=/home/gitpod/.ssh/id_rsa.pub -n airflow
+
+  # deploy
+  echo "### Deploying"
+  helm upgrade --install airflow apache-airflow/airflow -n airflow -f values.yaml --debug
+
+  # create yaml config
+  #helm show values apache-airflow/airflow > values.yaml
+
+  # help upgrade
+  # helm upgrade --install airflow apache-airflow/airflow -n airflow  \
+  #   -f values.yaml \
+  #   --debug
+
+  # if error ocurs
+  # kubectl create secret generic airflow-postgresql -n airflow --from-literal=password='postgres' --dry-run=client -o yaml | kubectl apply -f -
+  }
+
+forward(){
+  kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow
+}
+case $1 in
+  setup_cluster)
+    setup_cluster
+    ;;
+  forward)
+    forward
+    ;;
+  *)
+    echo "Usage: $0 {setup_cluster, forward}"
+    ;;
+esac
