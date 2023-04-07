866ffa3eca8b9e0d3fe69a44ec89a5c2be06c524
Kattson Bastos <kattsonbastos@gmail.com>
2023-04-07 14:19:24 +0000

    feat: fix: removing example dags

--- src/setup.sh
+++ src/setup.sh
@@ -13,7 +13,7 @@ install() {
 
     #heml
     echo "## Installing helm.."
-    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    #curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
     chmod 700 get_helm.sh
     ./get_helm.sh
 
--- src/setup_cluster.sh
+++ src/setup_cluster.sh
@@ -1,69 +1,64 @@
 
-# setup_cluster(){
-#   # set project
+setup_cluster(){
+   # set project
 
-echo "### Creating Cluster.."
-gcloud config set project k-practices
+  echo "### Creating Cluster.."
+  gcloud config set project k-practices
 
-# gcloud services list --available | grep container
-gcloud services enable container.googleapis.com
+  # gcloud services list --available | grep container
+  gcloud services enable container.googleapis.com
 
-gcloud container clusters create airflow-cluster \
---machine-type n1-standard-2 \
---num-nodes 1 \
---region "us-east1"
+  gcloud container clusters create airflow-cluster \
+  --machine-type n1-standard-2 \
+  --num-nodes 1 \
+  --region "us-east1"
 
-# authenticate kubectl
-echo "### Getting creds kb"
-gcloud container clusters get-credentials airflow-cluster --region "us-east1"
+  # authenticate kubectl
+  echo "### Getting creds kb"
+  gcloud container clusters get-credentials airflow-cluster --region "us-east1"
 
-# create a namespace
-kubectl create namespace airflow
+  # create a namespace
+  kubectl create namespace airflow
 
-# deploy airfflow
-helm repo add apache-airflow https://airflow.apache.org
+  # deploy airfflow
+  helm repo add apache-airflow https://airflow.apache.org
 
-# list repo
-helm repo list
+  # list repo
+  helm repo list
 
-# forward port
-# kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow
+  # forward port
+  # kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow
 
-echo "### Adding SSH"
+  echo "### Adding SSH"
 
-mkdir ~/.ssh
+  mkdir ~/.ssh
 
-#mv ./id_rsa.pub ~/.ssh
+  #mv ./id_rsa.pub ~/.ssh
 
-kubectl create secret generic airflow-gke-git-secret --from-file=gitSshKey=/home/gitpod/.ssh/id_rsa.pub -n airflow
+  kubectl create secret generic airflow-gke-git-secret --from-file=gitSshKey=/home/gitpod/.ssh/id_rsa.pub -n airflow
 
-# deploy
-echo "### Deploying"
-helm upgrade airflow apache-airflow/airflow -n airflow -f values.yaml --debug --timeout 10m30s
+  # deploy
+  echo "### Deploying"
+  helm upgrade airflow apache-airflow/airflow -n airflow -f values.yaml --debug --timeout 10m30s
 
-  # create yaml config
-  #helm show values apache-airflow/airflow > values.yaml
+    # create yaml config
+    #helm show values apache-airflow/airflow > values.yaml
 
-  # help upgrade
-  # helm upgrade --install airflow apache-airflow/airflow -n airflow  \
-  #   -f values.yaml \
-  #   --debug
+    # help upgrade
+    # helm upgrade --install airflow apache-airflow/airflow -n airflow  \
+    #   -f values.yaml \
+    #   --debug
 
-  # if error ocurs
-  # kubectl create secret generic airflow-postgresql -n airflow --from-literal=password='postgres' --dry-run=client -o yaml | kubectl apply -f -
-#   }
+    # if error ocurs
+    # kubectl create secret generic airflow-postgresql -n airflow --from-literal=password='postgres' --dry-run=client -o yaml | kubectl apply -f -
+}
 
-# forward(){
-#   kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow
-# }
-# case $1 in
-#   setup_cluster)
-#     setup_cluster
-#     ;;
-#   forward)
-#     forward
-#     ;;
-#   *)
-#     echo "Usage: $0 {setup_cluster, forward}"
-#     ;;
-# esac
+
+case $1 in
+  setup_cluster)
+    setup_cluster
+    ;;
+  *)
+    echo "Usage: $0 {setup_cluster}"
+    ;;
+esac
